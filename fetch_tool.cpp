19d6128292147289d48496132680848e959ec7d0
Lauri Kivimäki <laurikivimaki012@proton.me>
2025-01-15 12:12:11 +0200

    added os version

--- fetch_tool.cpp
+++ fetch_tool.cpp
@@ -1,6 +1,18 @@
+/* to do list
+1. make a gpu detector
+2. make a linux logo
+3. make a ram detector
+*/
+
+
+
+
 #include <iostream>
 #include <string>
 #include <fstream>
+
+//function that checks the os version
+std::string os_info(const std::string& os);
 
 //function that checks the cpu info
 std::string cpu (const std::string& cpu);
@@ -9,24 +21,42 @@ std::string cpu (const std::string& cpu);
 int cores_amount (const int& core_amount1); 
 
 int main (){
-    std::string os;
-    std::string ram;
-    std::string gpu;
 
-
+    
+    std::string os = os_info(os);
     std::string cpu_info = cpu(cpu_info);
     int core_amount1 = cores_amount(core_amount1);
 
+    std::cout << os << "\n";
     std::cout << "cpu " << cpu_info << "\n";
-    std::cout << "cores: " << core_amount1 << std::endl;
+    std::cout << "cpu-threads: " << core_amount1 << std::endl;
 
+}
+
+std::string os_info(const std::string& os){
+    std::ifstream infile("/etc/os-release");
+
+    if (!infile) {
+       throw std::runtime_error("Fatal: OS info could not be accessed\n"); 
+    }
+
+    std::string line;
+    std::string start_text="PRETTY_NAME";
+
+    while (std::getline(infile, line)) {
+        if (line.rfind(start_text,0) == 0) {
+            return line;
+        }
+    }
+
+    throw std::runtime_error("Cpu info file '/proc/cpuinfo' is not written as should");
 }
 
 std::string cpu (const std::string& cpu){
     std::ifstream infile("/proc/cpuinfo"); ///open the file that has the cpu info
 
     if (!infile) {
-        throw std::runtime_error("Cpu info not accesed"); //if you cant open the file, quit
+        throw std::runtime_error("Cpu info not accesed\n"); //if you cant open the file, quit
     }
 
     std::string line;
