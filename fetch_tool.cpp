ecc3a51e904f680fa7ab454f2e616ebd0c22f5d7
Lauri Kivimäki <laurikivimaki012@proton.me>
2025-01-17 11:35:03 +0200

    Tried making the gpu detector

--- fetch_tool.cpp
+++ fetch_tool.cpp
@@ -10,6 +10,9 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 //function that checks the os version
 std::string os_info(const std::string& os);
@@ -18,7 +21,9 @@ std::string os_info(const std::string& os);
 std::string cpu (const std::string& cpu);
 
 //function to get the core amount
-int cores_amount (const int& core_amount1); 
+int cores_amount (const int& core_amount1);
+
+std::string gpu (const std::string& gpuinfo);
 
 int main (){
 
@@ -26,10 +31,13 @@ int main (){
     std::string os = os_info(os);
     std::string cpu_info = cpu(cpu_info);
     int core_amount1 = cores_amount(core_amount1);
+    std::string gpuinfo = gpu(gpuinfo);
 
     std::cout << os << "\n";
     std::cout << "cpu " << cpu_info << "\n";
-    std::cout << "cpu-threads: " << core_amount1 << std::endl;
+    std::cout << "cpu-threads: " << core_amount1 << "\n";
+    std::cout << "GPU: " << gpuinfo << std::endl;
+    return 0;
 
 }
 
@@ -77,19 +85,41 @@ std::string cpu (const std::string& cpu){
 
 int cores_amount (const int& core_amount1){   
     std::ifstream infile("/proc/cpuinfo"); ///open the file that has the cpu info
-        if (!infile) {
-          throw std::runtime_error("Cpu info not accesed"); //if you cant open the file, quit
-        }
-        int amount=0;
-        std::string line;
-        std::string cores="processor";
+    if (!infile) {
+        throw std::runtime_error("Cpu info not accesed"); //if you cant open the file, quit
+    }
+    int amount=0;
+    std::string line;
+    std::string cores="processor";
 
-        while (std::getline(infile, line)){
-            if (line.rfind(cores, 0) == 0) {
-                amount++;
-            }
+    while (std::getline(infile, line)){
+        if (line.rfind(cores, 0) == 0) {
+            amount++;
         }
-        return amount;
+    }
+    return amount;
 
    
-}    
+}
+
+std::string gpu (const std::string& gpuinfo) {
+    system("touch /tmp/gpu.txt");
+    system("lshw -quiet -C display | grep 'product' >> /tmp/gpu.txt");
+
+    std::ifstream infile("/tmp/gpu.txt");
+    if (!infile) {
+        throw std::runtime_error("Gpu info not found");
+    }
+
+    std::string GPU_info;
+    
+    if (infile.is_open()) {
+        std::string GPU_substr = GPU_info.substr(GPU_info.find(":") +1);
+    }
+    else {
+        throw std::runtime_error("file not open");
+    }
+    system("rm /tmp/gpu.txt");
+
+    return GPU_info; 
+}
